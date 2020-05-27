ef5294b325ec6c017a72986768fee0036e05bfc8
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-27 16:26:52 +0530

    Testing uploading images

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
+import os
 app = Flask(__name__)
 CORS(app)
+
+app.config["IMAGE_UPLOADS"] = "./Uploads"
 api = Api(app)
 
-class HelloWorld(Resource):
+class FaceDetection(Resource):
     def get(self):
         return { 'about': 'Hello' }
     def post(self):
-        print(request.files)
         if request.files:
-            print('here i am')
             image = request.files["images"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
             print(image)
             return image
         return "Ayyyy"
-api.add_resource(HelloWorld, '/')
+api.add_resource(FaceDetection, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
