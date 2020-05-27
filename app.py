670c12b04ffcc5f344026aa51edd5d75c90c86ba
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-27 17:25:37 +0530

    Adding bug fix

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import os
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('./Uploads/blob')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 app = Flask(__name__)
 CORS(app)
 
@@ -17,6 +23,14 @@ class FaceDetection(Resource):
             image = request.files["images"]
             # image.save(os.path.join(MYDIR + "/" + app.config["IMAGE_UPLOADS"], image.filename))
             image.save(MYDIR + "/" + app.config["IMAGE_UPLOADS"])
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            if faces is not '':
+                print(True)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+            cv2.waitKey()
             return "Image Uploaded successfully"
         else:
             return "Error uploading File"
