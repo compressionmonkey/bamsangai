f125ffd6262804a7e938fe4229343a4701c218a6
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-28 16:47:59 +0530

    Testing

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_restful import Resource, Api
 import random
 from flask_cors import CORS
 import os
-# from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey
-from pathlib import Path
+from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey
 
 
 app = Flask(__name__)
@@ -15,7 +14,7 @@ api = Api(app)
 MYDIR = os.path.dirname(__file__)
 app.config["IMAGE_UPLOADS"] = MYDIR+"/Uploads"
 
-# face_cascade = CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 class FaceDetection(Resource):
@@ -27,23 +26,16 @@ class FaceDetection(Resource):
             image = request.files["images"]
             print("opopopop",image)
             image.save(os.getcwd()+"/Uploads/"+ str(name)+".jpg")
-            #image.save(MYDIR + "/" + app.config["IMAGE_UPLOADS"],image.filename)
-            # entries = Path(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + "/")
-            # for entry in entries.iterdir():
-            #     print(entry.name)
-            # with os.scandir(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + "/") as entries:
-            #     for entry in entries:
-            #         print("here is ",entry.name)
-            # img = imread(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + image.filename)
-            # gray = cvtColor(img, COLOR_BGR2GRAY)
-            # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            # if faces is not '':
-            #     print(True)
-            #
-            # for (x, y, w, h) in faces:
-            #     rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-            #
-            # waitKey()
+            img = imread(os.getcwd()+"/Uploads/"+ str(name)+".jpg")
+            gray = cvtColor(img, COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            if faces is not '':
+                print(True)
+
+            for (x, y, w, h) in faces:
+                rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+            waitKey()
             return "Image Uploaded successfully"
         else:
             return "Error uploading File"
