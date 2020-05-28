dab802dbb79a483cfe65fb3568b4e0b2bb78d629
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-28 14:23:53 +0530

    testing

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 import os
 # from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey
+from pathlib import Path
+
 
 app = Flask(__name__)
 CORS(app)
@@ -23,9 +25,12 @@ class FaceDetection(Resource):
             image = request.files["images"]
             # image.save(os.path.join(MYDIR + "/" + app.config["IMAGE_UPLOADS"], image.filename))
             image.save(MYDIR + "/" + app.config["IMAGE_UPLOADS"])
-            with os.scandir(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + "/") as entries:
-                for entry in entries:
-                    print("here is ",entry.name)
+            entries = Path(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + "/")
+            for entry in entries.iterdir():
+                print(entry.name)
+            # with os.scandir(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + "/") as entries:
+            #     for entry in entries:
+            #         print("here is ",entry.name)
             # img = imread(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + image.filename)
             # gray = cvtColor(img, COLOR_BGR2GRAY)
             # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
