79140d349067fcc5baf6de58a098eb198187c0b7
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-28 13:45:38 +0530

    Testing cv2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import os
-import cv2
+from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey
 
 app = Flask(__name__)
 CORS(app)
@@ -12,7 +12,7 @@ api = Api(app)
 MYDIR = os.path.dirname(__file__)
 app.config["IMAGE_UPLOADS"] = "Uploads"
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 class FaceDetection(Resource):
@@ -23,16 +23,16 @@ class FaceDetection(Resource):
             image = request.files["images"]
             # image.save(os.path.join(MYDIR + "/" + app.config["IMAGE_UPLOADS"], image.filename))
             image.save(MYDIR + "/" + app.config["IMAGE_UPLOADS"])
-            img = cv2.imread(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + image.filename)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = imread(MYDIR + "/" + app.config["IMAGE_UPLOADS"] + image.filename)
+            gray = cvtColor(img, COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
             if faces is not '':
                 print(True)
 
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
-            cv2.waitKey()
+            waitKey()
             return "Image Uploaded successfully"
         else:
             return "Error uploading File"
