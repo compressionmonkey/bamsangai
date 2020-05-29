1ed6edc25daf49b025ac233aa1af9cbffa73fa40
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-29 16:18:21 +0530

    Testing facial recognition

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class FaceDetection(Resource):
             img = imread(os.getcwd()+"/Uploads/"+ str(name)+".jpg")
             gray = cvtColor(img, COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            print("what is faces ",faces)
+            print("what is faces ",type(faces))
             if faces is not ():
                 return "You are a human Being. Enjoy your login"
 
