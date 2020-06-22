19c11b1b8fa40bddc0459dd527e2e2f8a4ea0ece
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-06-22 18:23:28 +0530

    fixed face detection

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Resource, Api
 import random
 from flask_cors import CORS
 import os
-from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey
-from pricepredictor import predict_price
+from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, waitKey, imshow
+# from pricepredictor import predict_price
 
 app = Flask(__name__)
 CORS(app)
@@ -26,25 +26,22 @@ class FaceDetection(Resource):
             image = request.files["images"]
             image.save(os.getcwd()+"/Uploads/"+ str(name)+".jpg")
             img = imread(os.getcwd()+"/Uploads/"+ str(name)+".jpg")
+            print(img)
             gray = cvtColor(img, COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            # print("faces <<<<< ", all(faces))
-            # print("what is faces ",faces.size) faces.size != 0 or
-            # gotta figure out how to check if numpy and tuple are empty
-            if faces is not '':
-                return "You are a human Being. Enjoy your login"
 
             for (x, y, w, h) in faces:
                 rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-
+                return "You are a human Being. Enjoy your login"
             waitKey()
+            return "no face detected!", 400
         else:
-            return "You are not a human being"
+            return "Please upload file"
 
 class PricePrediction(Resource):
     def get(self):
         return predict_price('Feb 20, 2018', 1000)
 api.add_resource(FaceDetection, '/')
-api.add_resource(PricePrediction, '/priceprediction')
+# api.add_resource(PricePrediction, '/priceprediction')
 if __name__ == '__main__':
     app.run(debug=True)
